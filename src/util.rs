06a1ed4579ead5e0c58667f7ffed8cb7df4ec735
Michael Richter <mr@osor.de>
2024-01-10 14:49:51 +0100

    Use io::Result

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,12 @@
-
+use std::mem;
 use std::slice::from_raw_parts_mut;
 use std::{
     cmp::min,
-    io::{Read, Write},
+    io::{Read, Result, Write},
 };
-use std::{io, mem};
 
 /// Read defined size of reader stream and copy to writer stream.
-pub fn copy_stream<R: Read, W: Write>(reader: &mut R, writer: &mut W, size: usize) -> Result<(), io::Error> {
+pub fn copy_stream<R: Read, W: Write>(reader: &mut R, writer: &mut W, size: usize) -> Result<()> {
     const BUF_SIZE: usize = 1024;
     let mut total = 0;
     let mut to_read = min(BUF_SIZE, size);
@@ -21,7 +20,7 @@ pub fn copy_stream<R: Read, W: Write>(reader: &mut R, writer: &mut W, size: usiz
     Ok(())
 }
 
-pub fn read_struct<R: Read, T: Sized>(reader: &mut R) -> io::Result<T> {
+pub fn read_struct<R: Read, T: Sized>(reader: &mut R) -> Result<T> {
     let mut obj: T = unsafe { mem::zeroed() };
     let size = mem::size_of::<T>();
     let buffer_slice = unsafe { from_raw_parts_mut(&mut obj as *mut _ as *mut u8, size) };
