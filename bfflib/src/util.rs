17e37a614e2e6f8ccbf4d8ffe450ebe060fde8e3
Michael Richter <mr@osor.de>
2024-05-21 11:23:04 +0200

    Remove copy_stream helper function (use io:copy)

--- bfflib/src/util.rs
+++ bfflib/src/util.rs
@@ -1,25 +1,7 @@
 use std::{fs, mem};
 use std::path::Path;
 use std::slice::from_raw_parts_mut;
-use std::{
-    cmp::min,
-    io::{Read, Result, Write},
-};
-
-/// Read defined `size` of `reader` stream and copy to `writer` stream.
-pub fn copy_stream<R: Read, W: Write>(reader: &mut R, writer: &mut W, size: usize) -> Result<()> {
-    const BUF_SIZE: usize = 1024;
-    let mut total = 0;
-    let mut to_read = min(BUF_SIZE, size);
-    while total < size {
-        let mut data = vec![0; to_read];
-        reader.read(&mut data)?;
-        writer.write_all(&data)?;
-        total += to_read;
-        to_read = min(BUF_SIZE, size - total);
-    }
-    Ok(())
-}
+use std::io::{Read, Result};
 
 /// Read binary data from a stream `reader` and map the bytes on the resulting
 /// struct. Target struct needs to be packed.
@@ -57,17 +39,6 @@ mod tests {
         pub a: u32,
         pub b: u16,
         pub c: u32,
-    }
-
-    #[test]
-    fn copy_stream_has_correct_size() -> Result<()> {
-        let mut stream = Cursor::new(b"abcdefghijklmnopqrstuvwxyz");
-        let mut result: Vec<u8> = vec![];
-
-        copy_stream(&mut stream, &mut result, 5)?;
-
-        assert_eq!(result, b"abcde");
-        Ok(())
     }
 
     #[test]
