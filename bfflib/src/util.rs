11f153200f5476b6dfbd50473be8861a4e7e42f1
Michael Richter <mr@osor.de>
2024-05-27 13:37:09 +0200

    Add create_dir_all tests

--- bfflib/src/util.rs
+++ bfflib/src/util.rs
@@ -30,6 +30,8 @@ pub(crate) fn create_dir_all<P: AsRef<Path>>(path: P) -> Result<()> {
 #[cfg(test)]
 mod tests {
     use std::io::Cursor;
+    use fs::File;
+    use tempfile::tempdir;
 
     use super::*;
 
@@ -51,5 +53,64 @@ mod tests {
         assert_eq!(result, expected);
 
         Ok(())
+    }
+
+    #[test]
+    fn test_create_dir_all_new() {
+        // Create a temporary directory path
+        let temp_dir = tempdir().unwrap();
+        let new_dir_path = temp_dir.path().join("test_create_dir_all_new");
+
+        // Ensure the directory does not exist
+        assert!(!new_dir_path.exists());
+
+        // Create the directory
+        create_dir_all(&new_dir_path).unwrap();
+
+        // Check if the directory exists
+        assert!(new_dir_path.exists());
+        assert!(new_dir_path.is_dir());
+    }
+
+    #[test]
+    fn test_create_dir_all_existing_dir() {
+        // Create a temporary directory path
+        let temp_dir = tempdir().unwrap();
+        let existing_dir_path = temp_dir.path().join("existing_dir");
+
+        // Create the directory initially
+        fs::create_dir(&existing_dir_path).unwrap();
+
+        // Ensure the directory exists
+        assert!(existing_dir_path.exists());
+        assert!(existing_dir_path.is_dir());
+
+        // Call create_dir_all on the existing directory
+        create_dir_all(&existing_dir_path).unwrap();
+
+        // Check if the directory still exists
+        assert!(existing_dir_path.exists());
+        assert!(existing_dir_path.is_dir());
+    }
+
+    #[test]
+    fn test_create_dir_all_replace_file() {
+        // Create a temporary directory path
+        let temp_dir = tempdir().unwrap();
+        let file_path = temp_dir.path().join("file");
+
+        // Create a file at the path
+        File::create(&file_path).unwrap();
+
+        // Ensure the file exists
+        assert!(file_path.exists());
+        assert!(file_path.is_file());
+
+        // Call create_dir_all on the path with the existing file
+        create_dir_all(&file_path).unwrap();
+
+        // Check if the file was replaced by a directory
+        assert!(file_path.exists());
+        assert!(file_path.is_dir());
     }
 }
